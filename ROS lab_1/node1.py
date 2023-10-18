1c9043358b70c8494468e3f0ddb6e8866a74c757
m-rehovz <111920894+m-rehovz@users.noreply.github.com>
2023-10-18 18:53:06 +0300

    Add files via upload
    Uploading laboratory work on the subject "ROS operating system"

---
+++ ROS lab_1/node1.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import rospy
+
+# это publisher
+
+from std_msgs.msg import Int32
+
+if __name__ == '__main__':
+    rospy.init_node("node1")
+    rospy.loginfo("Node 1  started...")
+    rate = rospy.Rate(10)
+
+    pub = rospy.Publisher("read_msg1", Int32, queue_size=10)
+    msg = Int32()
+    msg = 1
+
+
+    while not rospy.is_shutdown():
+        pub.publish(msg)
+        rospy.loginfo("Sending message to node2...")
+        rate.sleep()
+
+
+
+    # rospy.logwarn("Hello world worn")
+    # rospy.logerr("Hello world err")
+
